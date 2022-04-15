9af56f2bb6ca3aa6378bb46aaf8d75e5ab2fad45
arduinocc04 <arduinocc04@gmail.com>
2022-04-15 09:59:39 +0900

    Update flaskRun.py

--- flaskRun.py
+++ flaskRun.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import send_file
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/problems/<id>')
+def problems(id):
+    return send_file('problems/' + id, mimetype='image/png')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
